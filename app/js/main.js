6113377a30e57dd50493797d96d7a199a04dea77
Yuliia-777 <julichka-asia@outlook.com>
2020-07-08 18:58:02 +0300

    products-slider section done

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,28 @@
 $(function () {
 
+
+
     var mixer = mixitup('.products__inner');
+
+
+
+    $(".stars-rate").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+    });
+
+
+
+    $('.products-slider__inner').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows: false,
+        dots: true
+    });
+
+
 
 
 });
